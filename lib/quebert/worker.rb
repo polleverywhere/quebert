7d9c1ecbc3fec27228c9e60c934f1766012420ef
Steel Fu <steelfu@gmail.com>
2012-03-09 17:35:27 -0800

    [#25801467] Finish job first then exit

--- lib/quebert/worker.rb
+++ lib/quebert/worker.rb
@@ -10,18 +10,33 @@ module Quebert
     
     # Start the worker backend and intercept exceptions if a handler is provided
     def start
-      Signal.trap('TERM'){ stop }
-      
+      Signal.trap('TERM') { safe_stop }
+      Signal.trap('INT') { safe_stop }
+
       logger.info "Worker started with #{backend.class.name} backend\n"
-      while controller = backend.reserve do
+      while @controller = backend.reserve do      
         begin
-          controller.perform
+          @controller.perform
         rescue Exception => e
           exception_handler ? exception_handler.call(e) : raise(e)
-        end
+        end        
+        @controller = nil
+
+        stop if @terminate_sent
       end
     end
     
+    def safe_stop
+      if @terminate_sent
+        logger.info "Ok! I get the point. Shutting down immediately."
+        stop
+      else
+        logger.info "Finishing current job then shutting down."
+        @terminate_sent = true
+        stop unless @controller
+      end
+    end
+
     def stop
       logger.info "Worker stopping\n"
       exit 0
