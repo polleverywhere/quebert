2af58f0956889e4a0231f758ca974e167b9c300a
Mike Foley <mike@polleverywhere.com>
2013-08-28 09:25:01 -0700

    Sync#reserve simply returns a NullController to not do anything

--- lib/quebert/backend/sync.rb
+++ lib/quebert/backend/sync.rb
@@ -6,6 +6,11 @@ module Quebert
       def put(job, *args)
         Controller::Base.new(Job.from_json(job.to_json)).perform
       end
+
+      def reserve(*args, &block)
+        # reserve doesn't do anything in sync mode
+        @null_controller ||= Controller::NullController.new
+      end
     end
   end
 end
