a6850a42fb6b0a2cf8930725b7a25b91ee84ca38
Mike Foley <mike@polleverywhere.com>
2013-08-28 09:25:01 -0700

    Sync#reserve simply returns a NullController to not do anything

--- lib/quebert/controller.rb
+++ lib/quebert/controller.rb
@@ -3,7 +3,8 @@ module Quebert
   # handles exceptions that may be thrown by the Job and how the Job should
   # be put back on the queue, if at all.
   module Controller
-    autoload :Base,       'quebert/controller/base'
-    autoload :Beanstalk,  'quebert/controller/beanstalk'
+    autoload :Base,           'quebert/controller/base'
+    autoload :Beanstalk,      'quebert/controller/beanstalk'
+    autoload :NullController, 'quebert/controller/null_controller'
   end
 end
---
+++ lib/quebert/controller/null_controller.rb
@@ -0,0 +1,9 @@
+module Quebert
+  module Controller
+    # null object to stub a real controller and not do anything
+    class NullController
+      def perform
+      end
+    end
+  end
+end
